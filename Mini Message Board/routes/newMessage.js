b05ffbdd91b4bef00764c535c0fb1a5d984174fe
Nic-T <nicolastantau2003@gmail.com>
2021-12-31 13:44:42 +0200

    Stores the messages to a json file (emulating a database), it loads the messages, added functionality to read and add

--- Mini Message Board/routes/newMessage.js
+++ Mini Message Board/routes/newMessage.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const { type } = require("os");
 
 router.post("/", function (req, res) {
+  oldData = fs.readFileSync("messages.json");
+  oldData = JSON.parse(oldData);
+
   text = req.body.text;
   user = req.body.user;
 
   date = new Date();
 
   data = { text, user, date };
+
+  oldData.push(data);
+
+  oldData = JSON.stringify(oldData);
+
+  fs.writeFileSync("messages.json", oldData, "utf8");
+
   res.send(data);
-  console.log(data);
 });
 
 module.exports = router;
