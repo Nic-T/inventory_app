62dab36963eae65aad1581760d1c7b225d38463d
Nic-T <nicolastantau2003@gmail.com>
2022-02-01 11:41:52 +0200

    Completed the basic state of the controllers

--- Inventory Application/controllers/products.js
+++ Inventory Application/controllers/products.js
@@ -1,4 +1,3 @@
-const { where } = require("sequelize/dist");
 const { sequelize, Product } = require("../models");
 
 const getProducts = async (req, res) => {
@@ -11,9 +10,10 @@ const getProducts = async (req, res) => {
 };
 
 const getProduct = async (req, res) => {
-  const { id } = req.body;
+  const id = req.params.id;
   try {
     const product = await Product.findByPk(id);
+    res.json(product);
   } catch (err) {
     console.error(err);
   }
@@ -29,16 +29,42 @@ const createProduct = async (req, res) => {
   }
 };
 
-const updateProduct = (req, res) => {
+const updateProduct = async (req, res) => {
   try {
-    const { id, name, description, url } = req.body;
-    const oldProduct = await Product.findPyPk(id);
+    const { name, description, url } = req.body;
+    const id = req.params.id;
+    const updatedProduct = await Product.update(
+      {
+        description: description,
+        name: name,
+        url: url,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    res.json("Product updated succesfully");
   } catch (err) {
     console.error(err);
   }
 };
 
-const deleteProduct = (req, res) => {};
+const deleteProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.json("Product deleted sucesfully");
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
   getProducts,
