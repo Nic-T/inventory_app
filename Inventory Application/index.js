771db0f5fbe3e8ded727d9c67e7df04850c95618
Nic-T <nicolastantau2003@gmail.com>
2022-03-04 17:46:54 +0200

    changed the port

--- Inventory Application/index.js
+++ Inventory Application/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3100;
 let bodyParser = require("body-parser");
 
 const productRoutes = require("./routes/products");
