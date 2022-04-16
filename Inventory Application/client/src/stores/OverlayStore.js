2ca781176fa214c14e6a75118bfa9c6dab741eb4
Nic-T <nicolastantau2003@gmail.com>
2022-04-16 23:08:19 +0300

    This store keeps track of which modal will be opened

---
+++ Inventory Application/client/src/stores/OverlayStore.js
@@ -0,0 +1,5 @@
+import { writable } from 'svelte/store';
+
+export const addProductOverlay = writable(false);
+export const editProductOverlay = writable(false);
+export const viewProductOverlay = writable(false)
