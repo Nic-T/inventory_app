b9ef008a432fb04801c707562b023d8d1cc3f266
Nic-T <nicolastantau2003@gmail.com>
2022-04-23 02:37:18 +0300

    Added helmet, added the new routes, passport and jwt-token

--- Inventory Application/index.js
+++ Inventory Application/index.js
@@ -1,17 +1,22 @@
 const express = require("express");
+const helmet = require("helmet");
 const cors = require("cors");
-const app = express();
-const port = process.env.PORT || 3100;
 let bodyParser = require("body-parser");
+const app = express();
+
+const port = process.env.PORT || 3100;
 
 const productRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
 
 let db = require("./models");
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(helmet());
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", usersRoutes);
 
 db.sequelize.sync({ force: true }).then(function () {
   app.listen(port, () => {
