dafbbcdd586e30e6b0784e8959ee6fdfdd1a51a2
Nic-T <nicolastantau2003@gmail.com>
2022-04-24 00:06:22 +0300

    Other changes

--- Inventory Application/index.js
+++ Inventory Application/index.js
@@ -2,21 +2,24 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 let bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 const port = process.env.PORT || 3100;
 
 const productRoutes = require("./routes/products");
-const usersRoutes = require("./routes/users");
+const authRoutes = require("./routes/authentification");
 
 let db = require("./models");
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/products", productRoutes);
-app.use("/api/users", usersRoutes);
+app.use("/api/authentification", authRoutes);
 
 db.sequelize.sync({ force: true }).then(function () {
   app.listen(port, () => {
