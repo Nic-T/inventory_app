5f720aa5453c4a1a0017c5566491f81d9d655d27
Nic-T <nicolastantau2003@gmail.com>
2022-04-27 18:13:10 +0300

    client components that handle the login and the registration

---
+++ client/src/routes/login.svelte
@@ -0,0 +1,38 @@
+<script>
+
+    let username;
+    let password;
+
+    async function login() {
+
+        const res = await fetch(`http://localhost:3100/api/authentification/login`, {
+            method: 'POST',
+            mode: 'cors',
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            credentials: "include",
+            origin: 'http://localhost:3100',
+            body: JSON.stringify({username:username, password:password})
+        });
+        return res.json()
+
+    }
+
+    
+
+</script>
+
+<main>
+
+    <form on:submit|preventDefault={login}>
+    
+        <input type="text" name="username" placeholder="username" bind:value={username}/>
+        <input type="password" name="password" placegolder="password" bind:value={password}/>
+        <button type="submit">Login</button>
+
+    </form>
+
+</main>
---
+++ client/src/routes/register.svelte
@@ -0,0 +1,37 @@
+<script>
+
+    let username;
+    let password;
+    let email;
+
+    async function register() {
+
+        const res = await fetch(`http://localhost:3100/api/authentification/register`, {
+            method: 'POST',
+            mode: 'cors',
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            origin: 'http://localhost:3100',
+            body: JSON.stringify({username:username, password:password, email:email})
+        });
+        return res.json(res)
+
+    }
+
+</script>
+
+<main>
+
+    <form on:submit|preventDefault={register}>
+
+        <input type="text" name="username" placeholder="username" bind:value={username} />
+        <input type="email" name="email" placeholder="email" bind:value={email} />
+        <input type="password" name="password" placeholder="password" bind:value={password} />
+        <button type="submit">Register</button>
+
+    </form>
+
+</main>
