e5e791478522149e238d9c0e4d8442f97ff8f778
Nic-T <nicolastantau2003@gmail.com>
2022-04-27 18:17:15 +0300

    Added username

--- utils/jwt-helper.js
+++ utils/jwt-helper.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-function jwtTokens({ id, username, password }) {
-  const user = { id, username, password };
+function jwtTokens({ id, username, password, role }) {
+  const user = { id, username, password, role };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: "10m",
   });
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: "7d",
   });
-  return { accessToken, refreshToken };
+  return { accessToken, refreshToken, username };
 }
 
 module.exports = jwtTokens;
