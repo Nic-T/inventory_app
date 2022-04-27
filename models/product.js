96c706b923c7b55d999151f8d22dfdc219dee65a
Nic-T <nicolastantau2003@gmail.com>
2022-04-27 18:16:52 +0300

    Changed description to txt

--- models/product.js
+++ models/product.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
         primaryKey: true,
       },
       name: { type: DataTypes.STRING, allowNull: false },
-      description: { type: DataTypes.STRING, allowNull: false },
+      description: { type: DataTypes.TEXT, allowNull: false },
       category: {
         type: DataTypes.STRING,
         allowNull: false,
