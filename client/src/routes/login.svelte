64dca82bfd7243eee122a2dfe29005c5293659aa
Nic-T <nicolastantau2003@gmail.com>
2022-05-21 11:43:28 +0300

    Login now creates an access token

--- client/src/routes/login.svelte
+++ client/src/routes/login.svelte
@@ -1,27 +1,29 @@
 <script>
-
+    import { goto } from '$app/navigation';
+    import {tokenStore} from '../stores/tokenStore'
     let username;
     let password;
 
-    async function login() {
+        function login() {
 
-        const res = await fetch(`http://localhost:3100/api/authentification/login`, {
+        const res = fetch(`http://localhost:3100/api/authentification/login`, {
             method: 'POST',
             mode: 'cors',
 
             headers: {
                 'Content-Type': 'application/json',
-                'Accept': 'application/json'
+                "Accept": "*/*",
             },
-            credentials: "include",
             origin: 'http://localhost:3100',
             body: JSON.stringify({username:username, password:password})
-        });
-        return res.json()
-
+        }).then(function(response) {
+            return response.text();
+            }).then(function(data) {
+                tokenStore.set(data);
+                })
+                goto('/') 
     }
 
-    
 
 </script>
 
@@ -30,9 +32,10 @@
     <form on:submit|preventDefault={login}>
     
         <input type="text" name="username" placeholder="username" bind:value={username}/>
-        <input type="password" name="password" placegolder="password" bind:value={password}/>
+        <input type="password" name="password" placeholder="password" bind:value={password}/>
         <button type="submit">Login</button>
 
     </form>
+
 
 </main>
